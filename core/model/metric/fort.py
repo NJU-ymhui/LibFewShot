7f21124b52b5b8aa5799c164c2a79528bf563bb3
yuminghui <221870120@smail.nju.edu.cn>
2025-01-10 01:09:10 +0800

    add classifier fort.py and .yaml config files

---
+++ core/model/metric/fort.py
@@ -0,0 +1,18 @@
+from metric_model import *
+
+
+class FORT(MetricModel):
+    def __init__(self):
+        super(FORT, self).__init__()
+        # TODO
+
+    def set_forward(self, batch):
+        # TODO
+        return
+
+    def set_forward_loss(self, batch):
+        # TODO
+        return
+
+    # TODO other possible functions
+
